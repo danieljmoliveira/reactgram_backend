b1567bedbd296092dfa992dea0a6783b71bfe7bd
Daniel Mariano <danieljmo@gmail.com>
2023-12-05 20:59:10 -0300

    Method user update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,8 +1,9 @@
-const user = require("../models/User");
+const User = require("../models/User");
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+
+const  mongoose = require("mongoose");
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -84,8 +85,43 @@ const getCurrentUser = async (req, res) => {
 
 //Update an user
 const update = async (req, res) => {
-    res.send("Update");
+    const {name, password, bio} = req.body
+
+    let profileImage = null
+
+    if(req.file){
+        profileImage = req.file.filename
+    }
+
+    const reqUser = req.user
+
+    const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id)).select("-password");
+
+    if(name){
+        user.name = name
+    }
+
+    if(password){
+        // Generate password hash
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        user.password = passwordHash
+    }
+
+    if(profileImage){
+        user.profileImage = profileImage
+    }
+
+    if(bio){
+        user.bio = bio
+    }
+
+    await user.save()
+
+    res.status(200).json(user)
 };
+
 
 module.exports = {
     register,
