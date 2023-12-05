90339f37af90bb088846a2d351a8379887e1543d
Daniel Mariano <danieljmo@gmail.com>
2023-12-05 16:25:56 -0300

    Method user upload

--- middlewares/userValidations.js
+++ middlewares/userValidations.js
@@ -33,16 +33,30 @@ const loginValidation = () => {
     return [
         body("email")
             .isString()
-            .withMessage("O e-mail é obrigatório")
+            .withMessage("O e-mail é obrigatório.")
             .isEmail()
             .withMessage("insira um e-mail válido."),
         body("password")
             .isString()
-            .withMessage("A senha é obrigatória"),
+            .withMessage("A senha é obrigatória."),
+    ]
+}
+
+const userUpdateValidation = () => {
+    return [
+        body("name")
+            .optional()
+            .isLength({min: 3})
+            .withMessage("O nome precisa sde pelo menos 5 caracteres."),
+        body("password")
+            .optional()
+            .isLength({min: 5})
+            .withMessage("A senha precisa sde pelo menos 5 caracteres."),
     ]
 }
 
 module.exports = {
     userCreateValidation,
-    loginValidation
+    loginValidation,
+    userUpdateValidation,
 };
